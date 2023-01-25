6b9945dd56ef5e199d4c1a9c6b15b22dc5f34f12
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2023-01-25 13:50:07 -0500

    carList, CarSearch, CarValue updated

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,15 +2,22 @@ import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
   const handleCarDelete = (data) => {
     dispatch(removeCar(data));
   };
   const renderCars = cars.map((data) => {
+    const bold = name && data.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={data.id} className="panel">
+      <div key={data.id} className={`panel ${bold && "bold"} `}>
         <p>
           {data.name} - ${data.cost}
         </p>
--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeSearchTerm } from "../store/index";
+import { changeSearchTerm } from "../store";
 
 const CarSearch = () => {
   const dispatch = useDispatch();
